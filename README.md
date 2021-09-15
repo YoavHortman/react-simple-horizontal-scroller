2c53e4a890364c5773cc2dfb602a3f6a4653ae19
Yoav <yoavhortman5@gmail.com>
2021-09-15 17:29:11 +0100

    Slightly better demo and docs

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# A wrapper to manager horizontal scrolling
+# What is react-horizontal-scroller
+A 0 dependency, simple and lightweight wrapper, to manage horizontal scrolling and all of its caveats in a user friendly way.
 
 # Demo
 * https://yoavhortman.github.io/react-horizontal-scroller/
+
+
+# Features
+* Only shows scroll controls when actually required
+* Will scroll to selected item when mounting
+* Vertical mouse wheel scroll will translate to horizontal scroll
 
 ## Init
 * npm i
--- src/components/HorizontalScrollContainer/HorizontalScrollContainer.stories.tsx
+++ src/components/HorizontalScrollContainer/HorizontalScrollContainer.stories.tsx
@@ -11,21 +11,18 @@ const meta: Meta = {
   component: HorizontalScrollContainer
 }
 
-type TemplateProps = { children: any[], props: HorizontalScrollContainerProps }
+type TemplateProps = { initialItems: number[], props: HorizontalScrollContainerProps }
 
 export default meta;
-
-const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const children = items.map((item) => {
-  return <div style={{ flexShrink: 0, width: 100, height: 30, border: '1px solid black' }}>{item}</div>
-})
-
 
 
 // Create a master template for mapping args to render the Button component
 const Template: Story<TemplateProps> = (args: Partial<TemplateProps>) => {
   const [containerWidth, setContainerWidth] = React.useState(400);
   const [selected, setSelected] = React.useState(args.props?.selectedItemId)
+  const [items, setItems] = React.useState(args.initialItems ?? [])
+
+
 
   return <div style={{ width: '100%', display: "flex", flexDirection: 'column', alignItems: "center" }}>
 
@@ -34,6 +31,10 @@ const Template: Story<TemplateProps> = (args: Partial<TemplateProps>) => {
       <button onClick={() => setContainerWidth(Math.max(100, containerWidth - 100))}>- Width</button>
     </div>
 
+    <div style={{ padding: 8 }}>
+      <button onClick={() => setItems(items.concat(items.length))}>+ Item</button>
+      <button onClick={() => setItems(items.slice(0, -1))}>- Item</button>
+    </div>
 
     <div style={{ display: 'flex', overflow: 'hidden', width: containerWidth, border: '1px solid red', padding: 4 }}>
       <HorizontalScrollContainer {...args.props} selectedItemId={selected}>
@@ -52,10 +53,17 @@ const Template: Story<TemplateProps> = (args: Partial<TemplateProps>) => {
 
 export const Default = Template.bind({});
 Default.args = {
-  children: children
+  initialItems: [0, 1, 2]
 };
 
 export const WithPreselected = Template.bind({});
 WithPreselected.args = {
+  initialItems: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   props: { selectedItemId: '8' },
 };
+
+
+// export const WithDifferent = Template.bind({});
+// WithPreselected.args = {
+//   props: { selectedItemId: '8' },
+// };
